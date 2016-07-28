90bf5c62ae38cb90d713b56737cf7b2a53b86bb8
Cortney Gobern <CortneyGobern@Cortneys-MacBook-Pro.local>
2016-07-28 15:58:19 -0400

    timer function works

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,30 +1,57 @@
 $(document).ready(function() {
-	setTimeout(windowAlert, 2000);
-	function windowAlert() {
-		console.log("this is working!");
-
-	}
-//this hide all the classes below until 'start' button is pushed
+var correctAnswers= ["Raleigh", "Honolulu", "Tallahassee", "Austin", "Washington D.C.", "Richmond", "Harrisburg", "Sacramento", "Boston", "Denver"];
+var incorrectAnswers=0;		
+//this hides all the classes below until 'start' button is pushed
 	$(".timeLeft").hide();
-	$(".question").hide();
+	$("#panel").hide();
 	$(".done").hide();
 	$("#endGame").hide();
 //this will show the classes below until 'start' button is pushed
-	$('.start').on ('click', function() {
+		$('.start').on ('click', function() {
 		$(".timeLeft").show();
 		$(".done").show();
-		$(".question").show();
+		$("#panel").show();
 		$(".start").hide();
 		console.log("this function is working")
 	});
 	
 	$('.done').on('click', function(){
-		$(".question").hide();
+		$("#panel").hide();
 		$(".timeLeft").hide();
 		$("#endGame").show();
 		$(".done").hide();
 	});
+	//starts and stops timer
+	var theCount= 60;
+	$('.start').on('click', function startTimer() {
+		var gameTime=setInterval(decrement, 1000);
+		function decrement(){	
+  			theCount--;
+  			$('.timeLeft').html("Time remaining: " + theCount + " seconds");
+  			if (theCount < 0){
+  				$(".timeLeft").hide();
+  				$("#panel").hide();
+  				$("#endGame").show();
+  				$('.done').hide();
+  			};
+  		};
+	  	$('.done').on('click', function(){
+	  		clearInterval(gameTime);
+	  	//come back and make this timer stop when time runs out 
+	  	//not just when you click the 'done' button
+	  	});
+  	});		
+  	//this is checking each answer the user selected and sending it back
+  	
+
+  	// $.each($(input[name='answer']:"checked"), function() {
+
+  	// })
+
+
 });
+
+
 	// var questions = [
 	// { 	question: "1. What is the capital of North Carolina?",
 	// 	answers: ["Albany","Paris","Raleigh","None"],
